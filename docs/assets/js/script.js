68da9c9c928555a38873fd87d751a2b20dd52e57
Vikash <Vikashrajendran2806@gmail.com>
2022-02-11 01:03:23 +0530

    Feat: facicon add, url redirecter add in homepage

--- docs/assets/js/script.js
+++ docs/assets/js/script.js
@@ -1,26 +1,3 @@
-// redirect timer in Thanks page
-var counterElt = document.getElementById("counter");
-function decreaseCounter() {
-  var counter = Number(counterElt.textContent);
-  if (counter > 1) {
-    counterElt.textContent = counter - 1;
-  } else {
-    clearInterval(intervalId);
-    var titre = document.getElementById("redirect");
-    titre.click();
-  }
-}
-var intervalId = setInterval(decreaseCounter, 1000);
-
-// Submit Button audio effect
-let audio = document.querySelector('#audio');
-
-function playSuccess(){
-    audio.play();
-}
-
-
-
 // Google maps 
 
 function initMap() {
@@ -43,5 +20,30 @@ function initMap() {
     icon: image,
   });
 }
+
+
+
+
+// redirect timer in Thanks page
+var counterElt = document.getElementById("counter");
+function decreaseCounter() {
+  var counter = Number(counterElt.textContent);
+  if (counter > 1) {
+    counterElt.textContent = counter - 1;
+  } else {
+    clearInterval(intervalId);
+    var titre = document.getElementById("redirect");
+    titre.click();
+  }
+}
+var intervalId = setInterval(decreaseCounter, 1000);
+
+// Submit Button audio effect
+let audio = document.querySelector('#audio');
+
+function playSuccess(){
+    audio.play();
+}
+
 
 

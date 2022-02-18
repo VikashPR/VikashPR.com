9647801b1444d03ec4b24aad39615e4e237d44de
Vikash <Vikashrajendran2806@gmail.com>
2022-02-18 17:20:26 +0530

    Fix: gallery filter script removed

--- docs/assets/js/script.js
+++ docs/assets/js/script.js
@@ -1,29 +1,3 @@
-let sortBtn = document.querySelector('.filter-menu').children;
-let sortItem = document.querySelector('.filter-item').children;
-
-for(let i = 0; i < sortBtn.length; i++){
-    sortBtn[i].addEventListener('click', function(){
-        for(let j = 0; j< sortBtn.length; j++){
-            sortBtn[j].classList.remove('current');
-        }
-
-        this.classList.add('current');
-        
-
-        let targetData = this.getAttribute('data-target');
-
-        for(let k = 0; k < sortItem.length; k++){
-            sortItem[k].classList.remove('active');
-            sortItem[k].classList.add('delete');
-
-            if(sortItem[k].getAttribute('data-item') == targetData || targetData == "all"){
-                sortItem[k].classList.remove('delete');
-                sortItem[k].classList.add('active');
-            }
-        }
-    });
-}
-
 // Google maps 
 
 function initMap() {

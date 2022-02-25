70a11f89c5a13de2f6e862d4bfe4e8916e3c96d7
Vikash <Vikashrajendran2806@gmail.com>
2022-02-25 13:42:36 +0530

    Fix: form redirect enable in thanks.html

--- docs/assets/js/script.js
+++ docs/assets/js/script.js
@@ -24,19 +24,19 @@
 
 
 
-// // redirect timer in Thanks page
-// var counterElt = document.getElementById("counter");
-// function decreaseCounter() {
-//   var counter = Number(counterElt.textContent);
-//   if (counter > 1) {
-//     counterElt.textContent = counter - 1;
-//   } else {
-//     clearInterval(intervalId);
-//     var titre = document.getElementById("redirect");
-//     titre.click();
-//   }
-// }
-// var intervalId = setInterval(decreaseCounter, 1000);
+// redirect timer in Thanks page
+var counterElt = document.getElementById("counter");
+function decreaseCounter() {
+  var counter = Number(counterElt.textContent);
+  if (counter > 1) {
+    counterElt.textContent = counter - 1;
+  } else {
+    clearInterval(intervalId);
+    var titre = document.getElementById("redirect");
+    titre.click();
+  }
+}
+var intervalId = setInterval(decreaseCounter, 1000);
 
 
 // Submit Button audio effect

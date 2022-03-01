25edf3f77a1d5b5a79aa0b513e4e86b7c5b91ea3
Vikash <Vikashrajendran2806@gmail.com>
2022-03-01 11:57:24 +0530

    Fix: thanks page disable

--- docs/assets/js/script.js
+++ docs/assets/js/script.js
@@ -21,19 +21,19 @@
 // }
 
 // redirect timer in Thanks page
-var counterElt = document.getElementById('counter');
+// var counterElt = document.getElementById('counter');
 
-function decreaseCounter() {
-  var counter = Number(counterElt.textContent);
-  if (counter > 1) {
-    counterElt.textContent = counter - 1;
-  } else {
-    clearInterval(intervalId);
-    var titre = document.getElementById('redirect');
-    titre.click();
-  }
-}
-var intervalId = setInterval(decreaseCounter, 1000);
+// function decreaseCounter() {
+//   var counter = Number(counterElt.textContent);
+//   if (counter > 1) {
+//     counterElt.textContent = counter - 1;
+//   } else {
+//     clearInterval(intervalId);
+//     var titre = document.getElementById('redirect');
+//     titre.click();
+//   }
+// }
+// var intervalId = setInterval(decreaseCounter, 1000);
 
 // Submit Button audio effect
 let audioSuccess = document.querySelector('#audioSuccess');
